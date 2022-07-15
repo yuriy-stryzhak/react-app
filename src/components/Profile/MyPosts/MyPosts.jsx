839e2082af274e2d6c4ab706f6193a9249a0d8cc
ASTOUND\y.stryzhak <y.stryzhak@astoundcommerce.com>
2022-07-15 12:50:06 +0300

    Add and learn props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,17 +5,14 @@ import Post from './Post/Post';
 const MyPosts = () => {
     return (
         <div>
-            My MyPosts
+            My Posts
             <div>
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <Post />
-                <Post />
-                <Post />
-                <Post />
-                <Post />
+                <Post message="Hi, how are you?" />
+                <Post message="It's my first post" />
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
+
     return (
         <div className={s.item}>
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRv6dZvShX3_LSqgdJdwYZDL6MsrHYy5tjioGjaSFan1RfjUNJVi_A4PdSC-jUs_VxfDo&usqp=CAU" alt="ava" />
-            post 1
+            { props.message }
+            <div>
+                <span>like</span>
+            </div>
         </div>
     );
 }
